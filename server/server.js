8bbaddcf43242131b48e46a26ffc920fb0865251
SaravananManickam220592 <sarava220592@gmail.com>
2018-04-28 13:23:46 +0530

    Adding Get list of Todos

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
+//Route to add a new Todo
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -18,6 +19,15 @@ app.post('/todos', (req, res) => {
     }, (err) => {
         console.log('Error in Saving the Data');
         res.status(400).send(err);
+    });
+});
+
+//Route to get all the Todos
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    },(e)=>{
+        res.status(400).send(e);
     });
 });
 
