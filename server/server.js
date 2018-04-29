2dd751cac9d6fb5ce726c1de576429f08cc37da7
SaravananManickam2205 <sarava220592@gmail.com>
2018-04-29 21:24:13 +0530

    GET with ID

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -23,17 +24,34 @@ app.post('/todos', (req, res) => {
 });
 
 //Route to get all the Todos
-app.get('/todos',(req,res)=>{
-    Todo.find().then((todos)=>{
-        res.send({todos});
-    },(e)=>{
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
         res.status(400).send(e);
     });
 });
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo })
+
+    }).catch((e) => {
+        res.status(400).send();
+    });
+})
 
 //Configuring Express JS to listen to 3000 port
 app.listen(3000, () => {
     console.log('Started on Port 3000');
 });
 
-module.exports = {app};
+module.exports = { app };
