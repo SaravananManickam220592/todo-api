86b43914c2a30ffc32948a7f168dda2b6ad217bf
SaravananManickam2205 <sarava220592@gmail.com>
2018-05-01 12:03:24 +0530

    Adding MLab configs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+mongoose.connect('mongodb://sarodb:Sara2205@ds163689.mlab.com:63689/todoapidbsarava2205' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose };
