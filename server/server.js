3b6831204d7f13009d9a9af46ee8eccf701bb038
SaravananManickam2205 <sarava220592@gmail.com>
2018-05-07 23:20:40 +0530

    Private route changes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const _ = require('lodash');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -90,7 +91,7 @@ app.patch('/todos/:id', (req, res) => {
    
 });
 
-//Route to add a new Todo
+//Route to add a new User
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
@@ -102,6 +103,10 @@ app.post('/users', (req, res) => {
         console.log('Error in Saving the Data',err);
         res.status(400).send(err);
     });
+});
+
+app.get('/users/me',authenticate, (req,res) =>{
+    res.send(req.user);
 });
 
 //Configuring Express JS to listen to 3000 port or the Heroku Port
