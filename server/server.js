3368f779455c7ff46e7227359190ebec3cf3e63d
SaravananManickam2205 <sarava220592@gmail.com>
2018-05-19 14:36:44 +0530

    Making Todos Private

--- server/server.js
+++ server/server.js
@@ -15,9 +15,10 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 //Route to add a new Todo
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
     var todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator : req.user._id
     });
     todo.save().then((doc) => {
         res.send(doc);
@@ -28,8 +29,10 @@ app.post('/todos', (req, res) => {
 });
 
 //Route to get all the Todos
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
+app.get('/todos',authenticate, (req, res) => {
+    Todo.find({
+        _creator : req.user._id
+    }).then((todos) => {
         res.send({ todos });
     }, (e) => {
         res.status(400).send(e);
